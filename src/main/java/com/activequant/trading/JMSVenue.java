73414ccf423e1c6a5d17fd1598fe9b399e83304f
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-01-02 13:34:34 +0000

    adding stubs
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@30 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

---
+++ src/main/java/com/activequant/trading/JMSVenue.java
@@ -0,0 +1,12 @@
+package com.activequant.trading;
+
+/**
+ * Lightweight text-based Venue implementation. 
+ * Listens to and sends messages to a JMS queue.  
+ *  
+ * @author ustaudinger
+ *
+ */
+public class JMSVenue  {
+
+}
