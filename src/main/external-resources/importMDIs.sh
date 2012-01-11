8f515f2f43e64bab6f22824a84d75389ef8b2db9
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-01-11 13:23:58 +0000

    updating scripts, etc.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@106 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/external-resources/importMDIs.sh
+++ src/main/external-resources/importMDIs.sh
@@ -2,4 +2,4 @@
 # java -classpath .:aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar com.activequant.utils.ImportMarketDataInstrumentsCSV <CSVFILENAME> <SPRINGFILE> 
 
 CLASSPATH=.:$AQ_HOME/target/site/distribution/aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar
-java -classpath $CLASSPATH com.activequant.utils.ImportMarketDataInstrumentsCSV instruments.csv fwspring.xml
+java -classpath $CLASSPATH com.activequant.utils.ImportMarketDataInstrumentsCSV marketdatainstruments.csv fwspring.xml
