61b8968a6f9477a3f2051eb73aa550430db36f87
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-01-11 13:47:27 +0000

    cleanup
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@111 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/external-resources/importMarketData.sh
+++ src/main/external-resources/importMarketData.sh
@@ -2,4 +2,4 @@
 # java -classpath .:aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar com.activequant.utils.ImportMarketDataCSV <STARTFOLDER> <MDPROVIDE> <SPRINGFILE> <TIMEFRAME>
 
 CLASSPATH=.:$AQ_HOME/target/site/distribution/aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar
-java -classpath $CLASSPATH com.activequant.utils.ImportMarketDataCSV /home/aq2o/bb_data BB fwspring.xml EOD
+java -classpath $CLASSPATH com.activequant.utils.ImportMarketDataCSV md_yahoo fwspring.xml EOD
