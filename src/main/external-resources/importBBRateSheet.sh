4022865168ab557eb5bb8319e1d2622f528a365a
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-01-17 16:22:12 +0000

    small fix
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@155 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/external-resources/importBBRateSheet.sh
+++ src/main/external-resources/importBBRateSheet.sh
@@ -2,4 +2,4 @@
 # java -classpath .:aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar com.activequant.utils.ImportMarketDataCSV <STARTFOLDER> <MDPROVIDE> <SPRINGFILE> <TIMEFRAME>
 
 CLASSPATH=.:$AQ_HOME/target/site/distribution/aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar
-java -classpath $CLASSPATH com.activequant.utils.ImportPriceSheetCSV historical.csv BBGT fwspring.xml EOD
+java -classpath $CLASSPATH com.activequant.utils.ImportPriceSheet historical.csv BBGT fwspring.xml EOD
