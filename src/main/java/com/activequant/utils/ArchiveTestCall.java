78a4cbf46949ac2ad24f75a6393bdd914354a8b8
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-01-31 09:46:40 +0000

    better monitoring
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@183 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/java/com/activequant/utils/ArchiveTestCall.java
+++ src/main/java/com/activequant/utils/ArchiveTestCall.java
@@ -8,7 +8,6 @@ import com.activequant.archive.TSContainer;
 import com.activequant.archive.hbase.HBaseArchiveFactory;
 import com.activequant.domainmodel.TimeFrame;
 import com.activequant.domainmodel.TimeStamp;
-import com.activequant.exceptions.InvalidDate8Time6Input;
 
 /**
  * Helper app to do a simple test call to the archive. Useful for setting up monitoring tests. 
@@ -18,18 +17,19 @@ import com.activequant.exceptions.InvalidDate8Time6Input;
  */
 public class ArchiveTestCall {
 
-	public static void main(String[] args) throws InvalidDate8Time6Input, Exception{
+	public static void main(String[] args) throws Exception{
         
 	    HBaseArchiveFactory fac = new HBaseArchiveFactory(args[0]);
         IArchiveReader iar = fac.getReader(TimeFrame.EOD);
         IArchiveWriter iwr = fac.getWriter(TimeFrame.EOD);
         
         TimeStamp now = new TimeStamp(new Date());
-        iwr.write("TEST",now,"PX_SETTLE", Math.random());
+        double value = Math.random(); 
+        iwr.write("TEST",now,"PX_SETTLE", value);
         iwr.commit();
         
-        TSContainer container = iar.getTimeSeries("TEST", "PX_SETTLE", new TimeStamp(0L));
-        if(container!=null && container.timeStamps.length==0)
+        TSContainer container = iar.getTimeSeries("TEST", "PX_SETTLE", now);
+        if(container!=null && container.timeStamps.length==1 && container.values[0] == value)
         	System.out.println("All ok.");        
         else{
             System.out.println("Received: " + container.timeStamps.length + " values. ");
