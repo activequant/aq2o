0a56d0bdf3d071466ef4b9a09e775706ff3243b4
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-03-18 16:32:07 +0000

    cleanup
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@300 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/scripts/runserver.bat
+++
@@ -1,1 +0,0 @@
-java -classpath .;aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.AQ2Server
--- src/main/scripts/runserver.sh
+++ src/main/scripts/runserver.sh
@@ -1,2 +1,2 @@
 echo "Starting up AQ2o"
-java -classpath .:aq2o-2.0-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.AQ2Server
+java -classpath aq2o-2.1-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.AQ2Server
