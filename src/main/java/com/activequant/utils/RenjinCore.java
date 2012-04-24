59d94a83bc52afbd9796a61a08243fa7f3a76115
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-04-24 05:39:24 +0000

    #88
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@343 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

---
+++ src/main/java/com/activequant/utils/RenjinCore.java
@@ -0,0 +1,37 @@
+package com.activequant.utils;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class RenjinCore {
+
+	private ScriptEngine engine;
+
+	public RenjinCore() {
+		// create a script engine manager
+		ScriptEngineManager factory = new ScriptEngineManager();
+		// create an R engine
+		engine = factory.getEngineByName("Renjin");
+
+	}
+
+	public void execute(String cmd) throws ScriptException {
+		engine.eval(cmd);
+	}
+	
+	public void put(String key, Object object){
+		engine.put(key, object);
+	}
+
+	public Object get(String key){
+		return engine.get(key);
+	}
+	
+	public static void main(String[] args) throws ScriptException {
+		RenjinCore r = new RenjinCore();
+		r.put("a", 1);		
+		r.execute("b = c(a, sqrt(a + 1));");
+		System.out.println("Obj: " + r.get("b") + " " + r.get("b").getClass());		
+	}
+
+}
