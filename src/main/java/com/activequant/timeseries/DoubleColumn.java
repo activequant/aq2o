e53d030a172965c4d827fd4523c253208b8f75c0
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-05-09 10:31:51 +0000

    Merged aq2o timeseries [383]:[424] into trunk
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@425 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/java/com/activequant/timeseries/DoubleColumn.java
+++ src/main/java/com/activequant/timeseries/DoubleColumn.java
@@ -1,5 +1,24 @@
 package com.activequant.timeseries;
 
 public class DoubleColumn extends TypedColumn<Double> {
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Calculates the cumulated sum 
+	 * @return
+	 */
+	public DoubleColumn cumsum(){
+		DoubleColumn ret = new DoubleColumn();
+		Double currentVal = null; 
+		for(int i=0;i<super.size();i++){
+			if(currentVal==null)
+				currentVal = super.get(i);
+			if(currentVal!=null && i>0 && super.get(i)!=null){
+				currentVal = currentVal + super.get(i);
+			}
+			ret.add(currentVal);
+		}
+		return ret; 
+	}
+	
 }
