48627332bf3d34760972e3173e8fb0de9a8fd7e4
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-05-09 10:26:09 +0000

    Merged aq2o timeseries [383]:[422] into trunk
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@423 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/java/com/activequant/timeseries/TypedColumn.java
+++ src/main/java/com/activequant/timeseries/TypedColumn.java
@@ -2,11 +2,27 @@ package com.activequant.timeseries;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author GhostRider
+ *
+ * @param <T>
+ */
 public abstract class TypedColumn<T> extends ArrayList<T>{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * Overwrite null with a value. 
+	 * @param value
+	 */
+	public void replaceNull(T value){
+		for(int i=0;i<super.size();i++){
+			if(super.get(i)==null)super.set(i, value);
+		}
+	}
+	
 }
