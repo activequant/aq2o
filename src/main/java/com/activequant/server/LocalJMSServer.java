bc4b1b8ea080423d241561174e5d9e8013bac4aa
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-05-17 07:58:04 +0000

    #100
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@455 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/java/com/activequant/server/LocalJMSServer.java
+++ src/main/java/com/activequant/server/LocalJMSServer.java
@@ -9,7 +9,34 @@ public class LocalJMSServer {
     
     public void start(String hostname, int port) throws Exception{
         broker = new BrokerService();
+        broker.setUseJmx(false);        
         broker.addConnector("tcp://" + hostname + ":" + port);          
         broker.start();
+    }
+    
+    
+    /**
+     * Only needed in pure stanalone mode. 
+     */
+    public void runKeepaliveThread(){
+    	Thread t = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while(true)
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				
+			}        	
+        });
+    	t.start();
+    }
+    public static void main(String[] args) throws NumberFormatException, Exception{
+    	LocalJMSServer t = new LocalJMSServer();
+    	t.start(args[0], Integer.parseInt(args[1]));
+    	t.runKeepaliveThread();        
     }
 }
