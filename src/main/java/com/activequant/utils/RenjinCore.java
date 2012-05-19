15fd7c73becfb960f8b47217f272eecdbb184ab5
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-05-19 10:59:51 +0000

    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@470 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/java/com/activequant/utils/RenjinCore.java
+++ src/main/java/com/activequant/utils/RenjinCore.java
@@ -9,39 +9,70 @@ import org.apache.commons.vfs.VFS;
 import org.renjin.eval.Context;
 import org.renjin.script.RenjinScriptEngineFactory;
 import org.renjin.sexp.DoubleVector;
-
+/**
+ * 
+ * @author GhostRider
+ *
+ */
 public class RenjinCore {
 
 	private ScriptEngine engine;
 
+	/**
+	 * Initialized the renjin engine. 
+	 */
 	public RenjinCore() {
 		try {			
 			Context context = Context.newTopLevelContext();
-			context.getGlobals().setLibraryPaths("");
+			context.getGlobals().setLibraryPaths("");		
+			context.init();
 			engine = new RenjinScriptEngineFactory().getScriptEngine(context);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
+	/**
+	 * Executes an R command in the engine environment. 
+	 * @param cmd
+	 * @throws ScriptException
+	 */
 	public void execute(String cmd) throws ScriptException {
 		engine.eval(cmd);
 	}
 
+	/**
+	 * Puts an object into the engine's environment. 
+	 * @param key
+	 * @param object
+	 */
 	public void put(String key, Object object) {
 		engine.put(key, object);
 	}
 
+	
+	/**
+	 * Fetches an object from the engine's environment. 
+	 * @param key
+	 * @return a renjin object, for example Double Vector
+	 */
 	public Object get(String key) {
 		return engine.get(key);
 	}
 
+	/**
+	 * convenience method. fetches an object from engine env and casts it to double vector.  
+	 * 
+	 * @param key
+	 * @return
+	 */
 	public DoubleVector getDoubleVector(String key) {
 		return (DoubleVector) engine.get(key);
 	}
 	
 	/**
-	 * Returns a single value as double.
+	 * Returns a single value as double. Can break. 
+	 * 
 	 * @param key
 	 * @return
 	 */
@@ -51,6 +82,12 @@ public class RenjinCore {
 	
 	
 
+	/**
+	 * Just some basic fun testing method. 
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		RenjinCore r = new RenjinCore();
 		r.put("a", 1);
