3aa76c3fa1b6e3f480b1e513412edd901ce3da86
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-07-19 09:18:15 +0000

    fixing scripts
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@658 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- src/main/scripts/runlocaljmsserver.sh
+++ src/main/scripts/runlocaljmsserver.sh
@@ -1,2 +1,2 @@
 echo "Starting up AQ2o"
-java -classpath .:aq2o-2.1-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.LocalJMSServer localhost 61616
+java -classpath .:aq2o-2.2-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.LocalJMSServer localhost 61616
