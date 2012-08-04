fcdb255cc33e6a561f49de73dbb9cac8a0792bd0
vrakush <vrakush@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-08-04 20:20:15 +0000

    the column name of the stream column was hardcoded.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@723 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- base/src/main/java/com/activequant/backtesting/ArchiveStreamToMarketDataIterator.java
+++ base/src/main/java/com/activequant/backtesting/ArchiveStreamToMarketDataIterator.java
@@ -13,66 +13,72 @@ import com.activequant.interfaces.archive.IArchiveReader;
 /**
  * 
  * @author GhostRider
- *
+ * 
  */
-public class ArchiveStreamToMarketDataIterator extends StreamEventIterator<MarketDataEvent> {
+public class ArchiveStreamToMarketDataIterator extends
+		StreamEventIterator<MarketDataEvent> {
 
-    private String mdiId, tdiId; 
-    private MultiValueTimeSeriesIterator streamIterator; 
-    private double[] bid, ask, bidQ, askQ;
-    private double quantityOverride = 0.0;
-    MarketDataSnapshot mds = new MarketDataSnapshot();
+	private String mdiId, tdiId;
+	private MultiValueTimeSeriesIterator streamIterator;
+	private double[] bid, ask, bidQ, askQ;
+	private double quantityOverride = 0.0;
+	private String bidQuantityKey = "BIDQUANTITY";
+	private String askQuantityKey = "ASKQUANTITY";
+	MarketDataSnapshot mds = new MarketDataSnapshot();
 
-    public ArchiveStreamToMarketDataIterator(String mdiId, TimeStamp startTime, TimeStamp endTime, IArchiveReader archiveReader) throws Exception {
-        this.mdiId = mdiId;
-        this.tdiId = mdiId; 
-        this.streamIterator = archiveReader.getMultiValueStream(mdiId, startTime, endTime);
-        bid = new double[1];
-        ask = new double[1];
-        bidQ = new double[1];
-        askQ = new double[1];          
-    }
-    
-    public ArchiveStreamToMarketDataIterator(String mdiId, String tdiId, TimeStamp startTime, TimeStamp endTime, IArchiveReader archiveReader) throws Exception {
-        this.mdiId = mdiId;
-        this.streamIterator = archiveReader.getMultiValueStream(mdiId, startTime, endTime);
-        bid = new double[1];
-        ask = new double[1];
-        bidQ = new double[1];
-        askQ = new double[1];          
-        this.tdiId = tdiId; 
-    }
-    
-    
+	public ArchiveStreamToMarketDataIterator(String mdiId, TimeStamp startTime,
+			TimeStamp endTime, IArchiveReader archiveReader) throws Exception {
+		this.mdiId = mdiId;
+		this.tdiId = mdiId;
+		this.streamIterator = archiveReader.getMultiValueStream(mdiId,
+				startTime, endTime);
+		bid = new double[1];
+		ask = new double[1];
+		bidQ = new double[1];
+		askQ = new double[1];
+	}
 
-    @Override
-    public boolean hasNext() {
-       return streamIterator.hasNext();
-    }
+	public ArchiveStreamToMarketDataIterator(String mdiId, String tdiId,
+			TimeStamp startTime, TimeStamp endTime, IArchiveReader archiveReader)
+			throws Exception {
+		this.mdiId = mdiId;
+		this.streamIterator = archiveReader.getMultiValueStream(mdiId,
+				startTime, endTime);
+		bid = new double[1];
+		ask = new double[1];
+		bidQ = new double[1];
+		askQ = new double[1];
+		this.tdiId = tdiId;
+	}
 
-    @Override
-    public MarketDataEvent next() {
-        Tuple<TimeStamp, Map<String, Double>> valueMap = streamIterator.next();
-        if(valueMap.getB().containsKey("BID"))bid[0] = valueMap.getB().get("BID");
-        if(valueMap.getB().containsKey("ASK"))ask[0] = valueMap.getB().get("ASK");
-        if(valueMap.getB().containsKey("BIDQUANTITY"))bidQ[0] = valueMap.getB().get("BIDQUANTITY");
-        if(valueMap.getB().containsKey("ASKQUANTITY"))askQ[0] = valueMap.getB().get("ASKQUANTITY");
-        
-        // 
-        if(quantityOverride!=0.0){
-        	bidQ[0] = askQ[0] = quantityOverride;
-        }
+	@Override
+	public boolean hasNext() {
+		return streamIterator.hasNext();
+	}
 
-        mds.setMdiId(this.mdiId);
-        mds.setTdiId(this.tdiId);
-        mds.setAskPrices(ask);
-        mds.setBidPrices(bid);
-        mds.setAskSizes(askQ);
-        mds.setBidSizes(bidQ);
-        mds.setTimeStamp(valueMap.getA());
-        return mds; 
+	@Override
+	public MarketDataEvent next() {
+		Tuple<TimeStamp, Map<String, Double>> valueMap = streamIterator.next();
+		if (valueMap.getB().containsKey("BID")) bid[0] = valueMap.getB().get("BID");
+		if (valueMap.getB().containsKey("ASK")) ask[0] = valueMap.getB().get("ASK");
+		if (valueMap.getB().containsKey(bidQuantityKey)) bidQ[0] = valueMap.getB().get(bidQuantityKey);
+		if (valueMap.getB().containsKey(askQuantityKey)) askQ[0] = valueMap.getB().get(askQuantityKey);
 
-    }
+		//
+		if (quantityOverride != 0.0) {
+			bidQ[0] = askQ[0] = quantityOverride;
+		}
+
+		mds.setMdiId(this.mdiId);
+		mds.setTdiId(this.tdiId);
+		mds.setAskPrices(ask);
+		mds.setBidPrices(bid);
+		mds.setAskSizes(askQ);
+		mds.setBidSizes(bidQ);
+		mds.setTimeStamp(valueMap.getA());
+		return mds;
+
+	}
 
 	public double getQuantityOverride() {
 		return quantityOverride;
@@ -80,5 +86,21 @@ public class ArchiveStreamToMarketDataIterator extends StreamEventIterator<Marke
 
 	public void setQuantityOverride(double quantityOverride) {
 		this.quantityOverride = quantityOverride;
+	}
+
+	public String getBidQuantityKey() {
+		return bidQuantityKey;
+	}
+
+	public void setBidQuantityKey(String bidQuantityKey) {
+		this.bidQuantityKey = bidQuantityKey;
+	}
+
+	public String getAskQuantityKey() {
+		return askQuantityKey;
+	}
+
+	public void setAskQuantityKey(String askQuantityKey) {
+		this.askQuantityKey = askQuantityKey;
 	}
 }
