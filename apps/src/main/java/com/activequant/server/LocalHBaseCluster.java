95194e724b3aa962fbe596e1013476b8b557057d
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-09-10 07:51:36 +0000

    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@776 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/java/com/activequant/server/LocalHBaseCluster.java
+++ apps/src/main/java/com/activequant/server/LocalHBaseCluster.java
@@ -9,33 +9,41 @@ import org.apache.hadoop.hbase.master.HMaster;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
 import org.apache.hadoop.hbase.zookeeper.MiniZooKeeperCluster;
 
+/**
+ * plain and simple local hbase cluster code. Good for unit testing. Is also
+ * used on the AQ Master Server
+ * 
+ * 
+ * @author GhostRider
+ * 
+ */
 public class LocalHBaseCluster {
 
-    private String hostName = "localhost"; 
-    private int port = 2181; 
-    private String dataDir= "./"; 
-    public void start() throws Exception {
-        Configuration config = HBaseConfiguration.create();
-        // have to set a data directory. 
-        config.set(HConstants.ZOOKEEPER_QUORUM, hostName);
-        config.set(HConstants.HBASE_DIR, dataDir);    
-        // config.set(HConstants.CLUSTER_IS_LOCAL, "TRUE");
-        
-        
-        //
-        MiniZooKeeperCluster mzk = new MiniZooKeeperCluster(config);
-        mzk.setDefaultClientPort(port);
-        mzk.startup(new File("."+File.separator));
-        
-        HMaster master = new HMaster(config);
-        master.start();
-        
-        HRegionServer hrs = new HRegionServer(config);
-        HRegionServer.startRegionServer(hrs);
-    }
-    
-    public static void main(String[] args) throws Exception {
-    	new LocalHBaseCluster().start();
-    }
-    
+	private String hostName = "localhost";
+	private int port = 2181;
+	private String dataDir = "." + File.separator;
+
+	public void start() throws Exception {
+		Configuration config = HBaseConfiguration.create();
+		// have to set a data directory.
+		config.set(HConstants.ZOOKEEPER_QUORUM, hostName);
+		config.set(HConstants.HBASE_DIR, dataDir);
+		// config.set(HConstants.CLUSTER_IS_LOCAL, "TRUE");
+
+		//
+		MiniZooKeeperCluster mzk = new MiniZooKeeperCluster(config);
+		mzk.setDefaultClientPort(port);
+		mzk.startup(new File(dataDir));
+
+		HMaster master = new HMaster(config);
+		master.start();
+
+		HRegionServer hrs = new HRegionServer(config);
+		HRegionServer.startRegionServer(hrs);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new LocalHBaseCluster().start();
+	}
+
 }
