4d0567ba3362d39e96fe234852055a3e92b03b0b
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-09-19 19:40:27 +0000

    iadin
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@789 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/scripts/runserver.sh
+++ apps/src/main/scripts/runserver.sh
@@ -1,2 +1,2 @@
 echo "Starting up AQ2o"
-java -classpath .:aq2o-2.2-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.AQ2Server
+java -classpath ./:aq2o-apps-2.2-SNAPSHOT-jar-with-dependencies.jar com.activequant.server.AQ2Server
