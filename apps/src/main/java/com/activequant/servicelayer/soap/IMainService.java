1c5ad532a3bf204e8b5cb67f751bdf7323aa3d59
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2012-11-02 13:10:12 +0000

    fixes and latest version working.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@805 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/java/com/activequant/servicelayer/soap/IMainService.java
+++ apps/src/main/java/com/activequant/servicelayer/soap/IMainService.java
@@ -11,33 +11,43 @@ import javax.xml.ws.soap.SOAPBinding;
 
 import com.activequant.domainmodel.Instrument;
 import com.activequant.domainmodel.TimeFrame;
+import com.activequant.domainmodel.exceptions.DaoException;
+import com.activequant.domainmodel.exceptions.InvalidDataException;
+import com.activequant.dto.ClearedTradeDto;
+import com.activequant.dto.ClearerAccountStatementDto;
+import com.activequant.dto.OrderFillDto;
+import com.activequant.dto.PortfolioDto;
+import com.activequant.dto.PositionDto;
 
 @WebService
 @MTOM(enabled = false)
 @BindingType(SOAPBinding.SOAP11HTTP_BINDING)
 public interface IMainService {
-	
+
 	@WebMethod
 	public String[] instrumentKeys();
 
 	@WebMethod
-	public String[] findInstrumentKeys(@WebParam(name="regexPattern") String regexPattern);
+	public String[] findInstrumentKeys(
+			@WebParam(name = "regexPattern") String regexPattern);
 
-	
 	@WebMethod
 	public String[] mdiKeys();
-	
+
 	@WebMethod
-	public String[] findMdiKeys(@WebParam(name="regexPattern") String regexPattern);
-	
+	public String[] findMdiKeys(
+			@WebParam(name = "regexPattern") String regexPattern);
+
 	@WebMethod
 	public String[] tdiKeys();
-	
+
 	@WebMethod
-	public String[] findTdiKeys(@WebParam(name="regexPattern") String regexPattern);
-	
+	public String[] findTdiKeys(
+			@WebParam(name = "regexPattern") String regexPattern);
+
 	@WebMethod
-	public Instrument loadInstrument(@WebParam(name="PrimaryKey") String primaryKey);
+	public Instrument loadInstrument(
+			@WebParam(name = "PrimaryKey") String primaryKey);
 
 	@WebMethod
 	public int instrumentCount();
@@ -46,18 +56,55 @@ public interface IMainService {
 	public int mdiCount();
 
 	@WebMethod
-	public double[][] getTimeSeries(@WebParam(name="seriesId") String seriesId, @WebParam(name="seriesColumn") String column,
-			@WebParam(name="timeFrame") TimeFrame timeFrame, @WebParam(name="startTimeStamp") String date8Start, @WebParam(name="endTimeStamp") String date8End)
-			throws Exception;
+	public double[][] getTimeSeries(
+			@WebParam(name = "seriesId") String seriesId,
+			@WebParam(name = "seriesColumn") String column,
+			@WebParam(name = "timeFrame") TimeFrame timeFrame,
+			@WebParam(name = "startTimeStamp") String date8Start,
+			@WebParam(name = "endTimeStamp") String date8End) throws Exception;
 
 	@WebMethod
 	public void saveTimeSeriesValue(String seriesKey, TimeFrame timeFrame,
 			long nanoSeconds, String key, Object value) throws IOException;
 
 	@WebMethod
-	public void storeKeyVal(@WebParam(name="key") String key, @WebParam(name="value") String val);
-	
+	public void storeKeyVal(@WebParam(name = "key") String key,
+			@WebParam(name = "value") String val);
+
 	@WebMethod
-	public String fetchKeyVal(@WebParam(name="key") String key);
+	public String fetchKeyVal(@WebParam(name = "key") String key);
+
+	@WebMethod
+	public double testCall() throws Exception;
+
+	@WebMethod
+	public void addOrderFill(OrderFillDto dto) throws Exception;
+
+	@WebMethod
+	public void addClearedTrade(ClearedTradeDto dto) throws Exception;
+
+	@WebMethod
+	public void setSeriesValue(String portfolio, String seriesName,
+			long timeStampInNanos, double value);
+
+	@WebMethod
+	public void addPosition(PositionDto dto) throws Exception;
+
+
+
+	@WebMethod
+	public void addClearerAccountSnap(ClearerAccountStatementDto csdto)
+			throws InvalidDataException, DaoException;
+
+
+
+	@WebMethod
+	public void addPnS(String tradeableId, long date8,
+			String clearingAccountId, String currency, Double netAmount) throws DaoException;
+
+	@WebMethod
+	public void addPNL(String tradeableId, long date8,
+			String clearingAccountId, String currency, Double grossPNL,
+			Double netPNL) throws DaoException;
 
 }
--- apps/src/main/java/com/activequant/servicelayer/soap/MainService.java
+++ apps/src/main/java/com/activequant/servicelayer/soap/MainService.java
@@ -1,31 +1,66 @@
 package com.activequant.servicelayer.soap;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.regex.Pattern;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
 import javax.xml.ws.soap.MTOM;
 import javax.xml.ws.soap.SOAPBinding;
 
+import org.apache.log4j.Logger;
+
 import com.activequant.archive.TSContainer;
+import com.activequant.domainmodel.BloombergMarketDataInstrument;
 import com.activequant.domainmodel.Instrument;
 import com.activequant.domainmodel.TimeFrame;
 import com.activequant.domainmodel.TimeStamp;
+import com.activequant.domainmodel.backoffice.ClearedTrade;
+import com.activequant.domainmodel.backoffice.ClearerAccount;
+import com.activequant.domainmodel.backoffice.ClearerAccountSnap;
+import com.activequant.domainmodel.backoffice.OrderFill;
+import com.activequant.domainmodel.backoffice.PNL;
+import com.activequant.domainmodel.backoffice.PandS;
+import com.activequant.domainmodel.backoffice.PortfolioSnap;
+import com.activequant.domainmodel.backoffice.SubClearerAccount;
 import com.activequant.domainmodel.exceptions.DaoException;
+import com.activequant.domainmodel.exceptions.InvalidDataException;
+import com.activequant.dto.ClearedTradeDto;
+import com.activequant.dto.ClearerAccountStatementDto;
+import com.activequant.dto.DtoToDomainConv;
+import com.activequant.dto.OrderFillDto;
+import com.activequant.dto.PortfolioDto;
+import com.activequant.dto.PositionDto;
 import com.activequant.interfaces.archive.IArchiveFactory;
 import com.activequant.interfaces.archive.IArchiveWriter;
+import com.activequant.interfaces.dao.IAccountDao;
+import com.activequant.interfaces.dao.IClearedTradeDao;
+import com.activequant.interfaces.dao.IClearerAccountSnapDao;
 import com.activequant.interfaces.dao.IDaoFactory;
 import com.activequant.interfaces.dao.IInstrumentDao;
 import com.activequant.interfaces.dao.IMarketDataInstrumentDao;
+import com.activequant.interfaces.dao.IOrderFillDao;
+import com.activequant.interfaces.dao.IPNLDao;
+import com.activequant.interfaces.dao.IPandSDao;
 import com.activequant.interfaces.dao.IPerformanceReportDao;
+import com.activequant.interfaces.dao.IPortfolioDao;
+import com.activequant.interfaces.dao.IPortfolioSnapDao;
 import com.activequant.interfaces.dao.IReportDao;
+import com.activequant.interfaces.dao.ISubClearerAccountDao;
 import com.activequant.interfaces.dao.ITradeableInstrumentDao;
 import com.activequant.utils.Date8Time6Parser;
+import com.activequant.utils.worker.Worker;
+import com.activequant.utils.worker.WorkerThread;
 
 @WebService(endpointInterface = "com.activequant.servicelayer.soap.IMainService", serviceName = "MainService")
 @BindingType(SOAPBinding.SOAP11HTTP_BINDING)
@@ -38,7 +73,28 @@ public class MainService implements IMainService {
 	private IArchiveFactory archFac;
 	private IPerformanceReportDao perfDao;
 	private IReportDao reportDao;
-	
+
+
+	private DtoToDomainConv converter = new DtoToDomainConv();
+	private final IOrderFillDao ofDao;
+	private final IClearedTradeDao ctDao;
+	private final IAccountDao accDao;
+	private final ISubClearerAccountDao subClrAccDao;
+	private final IPortfolioSnapDao pSnapDao;
+	private final IPortfolioDao pDao;
+	private final IClearerAccountSnapDao caSnapDao;
+	private Logger log = Logger.getLogger(MainService.class);
+	private List<String> brokerIds = new ArrayList<String>();
+	private List<String> accountIds = new ArrayList<String>();
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+	LinkedBlockingQueue<PositionDto> posDtoQueue = new LinkedBlockingQueue<PositionDto>();
+	private WorkerThread<PositionDto> workerThread = null;
+
+	LinkedBlockingQueue<PortfolioDto> posDtoQueue2 = new LinkedBlockingQueue<PortfolioDto>();
+	private WorkerThread<PortfolioDto> workerThread2 = null;
+	private IPandSDao pandsDao;
+	private IPNLDao pnlDao;
 	// only for development purposes. 
 	private Map<String, String> inMemoryKeyValMap = new HashMap<String, String>();
 
@@ -48,7 +104,24 @@ public class MainService implements IMainService {
 		this.perfDao = daoFactory.perfDao();
 		this.reportDao = daoFactory.reportDao();
 		this.tdiDao = daoFactory.tradeableDao(); 
-		this.mdiDao = daoFactory.mdiDao(); 
+		this.mdiDao = daoFactory.mdiDao();
+		
+		
+		
+		ofDao = daoFactory.orderFillDao();
+		ctDao = daoFactory.clearedTradeDao();
+		accDao = daoFactory.accountDao();
+		pSnapDao = daoFactory.portfolioSnapDao();
+		pDao = daoFactory.portfolioDao();
+		subClrAccDao = daoFactory.subClearerAccountDao();
+		
+		caSnapDao = daoFactory.clearerAccountSnapDao();
+		
+		pnlDao = daoFactory.pnlDao();
+		pandsDao = daoFactory.pAndSDao();
+	
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
 	}
 
 	public String[] instrumentKeys() {
@@ -154,11 +227,18 @@ public class MainService implements IMainService {
 		w.commit();
 	}
 
+	
+	/**
+	 * Stores something in-memory
+	 */
 	@Override
 	public void storeKeyVal(String key, String val) {
 		inMemoryKeyValMap.put(key, val);
 	}
 
+	/**
+	 * Fetches from the in-memory key-value store. 
+	 */
 	@Override
 	public String fetchKeyVal(String key) {
 		return inMemoryKeyValMap.get(key);
@@ -241,6 +321,238 @@ public class MainService implements IMainService {
 		return null;
 	}
 
+	
+
+	public double testCall() {
+		return (int) (100 * Math.random());
+	}
+
+	
+	
+	/**
+	 * adds an order fill.
+	 * 
+	 */
+	@Override
+	public void addOrderFill(OrderFillDto dto) throws Exception {
+		OrderFill of = converter.convert(dto);
+		log.info("Adding order fill for " + dto.brokerAccountId);
+		try {
+			ofDao.update(of);
+
+			// also checking if we have an existing or a new mdi.
+			String tradId = dto.tradeableId;
+			//
+			//
+			if (mdiDao.findByProvId("BBGT", tradId) == null) {
+				BloombergMarketDataInstrument mdi = new BloombergMarketDataInstrument();
+				mdi.setMdProvider("BBGT");
+				mdi.setProviderSpecificId(tradId.toUpperCase());
+				mdiDao.update(mdi);
+			}
+
+		} catch (Exception ex) {
+			log.warn("Error while adding order fill: " + dto.toString(), ex);
+		}
+		log.debug("Adding done..");
+	}
+
+	/**
+	 * adds an order fill.
+	 * 
+	 */
+	@Override
+	public void addClearedTrade(ClearedTradeDto dto) throws Exception {
+		ClearedTrade ct = converter.convert(dto);
+		log.info("Adding cleared trade. " + dto.clearingAccountId);
+		try {
+			ctDao.update(ct);
+			System.out.println(ct.getId());
+		} catch (Exception ex) {
+			log.warn("Error while adding order fill: " + dto.toString(), ex);
+		}
+		log.debug("Adding done.");
+	}
+
+	@Override
+	public void setSeriesValue(@WebParam(name = "portfolio") String portfolio,
+			@WebParam(name = "seriesName") String seriesName,
+			@WebParam(name = "timeStampInNanos") long timeStampInNanos, @WebParam(name = "value") double value) {
+		String seriesId = "CUSTSERIES." + portfolio;
+		IArchiveWriter iaw = archFac.getWriter(TimeFrame.RAW);
+		iaw.write(seriesId, new TimeStamp(timeStampInNanos), seriesName, value);
+	}
+
+	@Override
+	/**
+	 * Method add positions. positions have to be added in their entirety.
+	 * 
+	 *  1) checks if we have such a clearer account, if not, create it. 
+	 *  2) checks if we have such a sub clearer account, if not, create it. 
+	 *  3) loads the up-to-dateportfolio snap for the handed in date. 
+	 *  3.1.) if no snap exists, create a new snap
+	 *  3.2.) if the loaded snap's timestamp in nanos does not match the incoming date, create a new snap with this particular snapdate. 
+	 *  Reason: it could be that we loaded an old snap, from the day before, therefore we have to compare the new snap's date with the old snap's date.   
+	 *   
+	 */
+	public void addPosition(PositionDto p) throws Exception {
+
+		if (workerThread == null) {
+			Worker<PositionDto> worker = new Worker<PositionDto>() {
+				@Override
+				public void process(PositionDto p) {
+					// find the corresponding portfolio.
+					String cAct = p.clearerAcctId;
+					String subAct = p.subAcctId;
+					SubClearerAccount sca;
+					try {
+						sca = checkClearerAccount(p.clearer, cAct, subAct);
+						Long date8 = p.positionDate8;
+						PortfolioSnap snap;
+						try {
+							snap = getSnapCache(sca.getId(), date8);
+							log.info("Adding position to snap (" + snap.getId() + "): " + p.toString());
+							snap.addPosition(p);
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					} catch (DaoException e1) {
+						e1.printStackTrace();
+					}
+					
+				}
+
+				@Override
+				public void queueEmpty() {
+					synchronized (portfolioSnapCache) {
+						Collection<PortfolioSnap> snaps = portfolioSnapCache.values();
+						for (PortfolioSnap snap : snaps) {
+							log.info("Saving position snap ...");
+							// ... and update it in the database.
+							try {
+								pSnapDao.delete(snap);
+								pSnapDao.create(snap);
+
+							} catch (DaoException e) {
+								e.printStackTrace();
+							}
+						}
+						// clear the snaps.
+						portfolioSnapCache.clear();
+					}
+				}
+			};
+			workerThread = new WorkerThread<PositionDto>(posDtoQueue, worker);
+			Thread t = new Thread(workerThread);
+			t.start();
+		}
+
+		log.info("Adding position  ... ");
+		posDtoQueue.add(p);
+
+	}
+
+	private PortfolioSnap getSnapCache(String ownerId, Long date8) throws ParseException {
+		synchronized (portfolioSnapCache) {
+			String key = ownerId + "." + date8;
+			if (!portfolioSnapCache.containsKey(key)) {
+				TimeStamp when = new TimeStamp(sdf.parse(date8.toString()));
+				PortfolioSnap snap = new PortfolioSnap();
+				snap.setOwnerObjectId(ownerId);		
+				snap.setTimeStampInNanoseconds(when.getNanoseconds());
+				snap.setCreationTime(date8);
+		
+				portfolioSnapCache.put(key, snap);
+			}
+			return portfolioSnapCache.get(key);
+		}
+	}
+
+	private Map<String, PortfolioSnap> portfolioSnapCache = new HashMap<String, PortfolioSnap>();
+
+	private PortfolioSnap getSnapCache2(String ownerId, Long date8) throws ParseException {
+		synchronized (portfolioSnapCache2) {
+			String key = ownerId + "." + date8;
+			if (!portfolioSnapCache2.containsKey(key)) {
+				TimeStamp when = new TimeStamp(sdf.parse(date8.toString()));
+				PortfolioSnap snap = new PortfolioSnap();
+				snap.setOwnerObjectId(ownerId);
+				snap = pSnapDao.loadSnapshot(snap.getNonUniqueID(), when);
+				if (snap == null || snap.getTimeStampInNanoseconds() != when.getNanoseconds()) {
+					// ok, the most up-to-date snap is not our position's snap.
+					snap = new PortfolioSnap();
+					snap.setTimeStampInNanoseconds(when.getNanoseconds());
+					// initialize it.
+					snap.setOwnerObjectId(ownerId);
+				}
+				portfolioSnapCache2.put(key, snap);
+			}
+			return portfolioSnapCache2.get(key);
+		}
+	}
+
+	private Map<String, PortfolioSnap> portfolioSnapCache2 = new HashMap<String, PortfolioSnap>();
+
+	private SubClearerAccount checkClearerAccount(@WebParam(name = "legalEntity") String legalEntity,
+			@WebParam(name = "accountId") String accountId, @WebParam(name = "subAccountId") String subAccountId) throws DaoException {
+		//
+		ClearerAccount ca = new ClearerAccount();
+		ca.setLegalEntity(legalEntity);
+		ca.setAccountId(accountId);
+		String id = ca.getId();
+
+		// check if we have such a clearer account.
+		if (accDao.load(id) == null)
+			accDao.create(ca);
+
+		SubClearerAccount sca = new SubClearerAccount();
+		sca.setClearerAcctObjId(id);
+		sca.setSubAcctId(subAccountId);
+		if (subClrAccDao.load(sca.getId()) == null)
+			subClrAccDao.create(sca);
+		return sca;
+	}
+
+
+	@Override
+	public void addClearerAccountSnap(ClearerAccountStatementDto csdto) throws InvalidDataException, DaoException{
+		//
+		ClearerAccountSnap c = converter.convert(csdto);
+		// create the snap.
+		caSnapDao.update(c);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public void addPnS(String tradeableId, long date8, String clearingAccountId, String currency, Double netAmount) throws DaoException{
+		PandS p = new PandS();
+		p.setTradeableId(tradeableId);
+		p.setDate8(date8);
+		p.setClearingAccount(clearingAccountId);
+		p.setCurrency(currency);
+		p.setNetAmount(netAmount);
+		
+		pandsDao.delete(p);
+		pandsDao.create(p);		
+	}
+
+
+	
+	public void addPNL(String tradeableId, long date8, String clearingAccountId, String currency, Double grossPNL, Double netPNL) throws DaoException{
+		PNL p = new PNL();
+		p.setTradeableId(tradeableId);
+		p.setDate8(date8);
+		p.setClearingAccountId(clearingAccountId);
+		p.setCurrency(currency);
+		p.setGrossPNL(grossPNL);
+		p.setNetPNL(netPNL);
+		pnlDao.delete(p);
+		pnlDao.create(p);
+	}
+
+	
 	
 	
 
