aa50bf1190b345dfd3a8bb59fa2922574d889fd1
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2013-01-03 15:39:58 +0000

    fixing fixing fixing.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@849 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/java/com/activequant/server/LocalHBaseCluster.java
+++ apps/src/main/java/com/activequant/server/LocalHBaseCluster.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.master.HMaster;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
 import org.apache.hadoop.hbase.zookeeper.MiniZooKeeperCluster;
+import org.apache.log4j.Logger;
 
 /**
  * plain and simple local hbase cluster code. Good for unit testing. Is also
@@ -23,7 +24,8 @@ public class LocalHBaseCluster {
 	private final String zookeeperHost;
 	private final int zookeeperPort;
 	private String dataDir = "." + File.separator;
-	private Properties properties = new Properties(); 
+	private Properties properties = new Properties();
+	private Logger log = Logger.getLogger(LocalHBaseCluster.class);
 	
 	public LocalHBaseCluster() {
 		zookeeperHost = "127.0.0.1";
@@ -41,24 +43,26 @@ public class LocalHBaseCluster {
 		// have to set a data directory.
 		config.set(HConstants.ZOOKEEPER_QUORUM, zookeeperHost);
 		config.set(HConstants.HBASE_DIR, dataDir);
+		config.set("hbase.master.info.port", "-10");
+		config.set("hbase.regionserver.info.port", "-10");
 		// config.set(HConstants.CLUSTER_IS_LOCAL, "TRUE");
 
+		log.info("Starting zookeeper.");
 		//
 		MiniZooKeeperCluster mzk = new MiniZooKeeperCluster(config);
 		mzk.setDefaultClientPort(zookeeperPort);
 		mzk.startup(new File(dataDir));
 		
 		// config.set("hbase.master.dns.interface", "eth0");
-		if(properties.containsKey("fixBuggyNetworking")){
+		if(properties.containsKey("fixBuggyNetworking") && properties.get("fixBuggyNetworking").equals("true")){
 			config.set("aqHostOverride", properties.getProperty("overrideHostName"));	
 		}
 				
-	    
-				
+		log.info("Starting hmaster.");				
 		HMaster master = new HMaster(config);
 		master.start();
 		
-
+		log.info("Starting regionserver.");
 		HRegionServer hrs = new HRegionServer(config);
 		HRegionServer.startRegionServer(hrs);
 	}
