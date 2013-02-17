06395a6cd6f110892dfb8d4dcab20b23f9778040
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2013-02-17 19:37:40 +0000

    doable.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@968 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/java/com/activequant/server/components/QuandlDownloaderComponent.java
+++ apps/src/main/java/com/activequant/server/components/QuandlDownloaderComponent.java
@@ -6,10 +6,12 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import com.activequant.archive.hbase.HBaseArchiveFactory;
 import com.activequant.component.ComponentBase;
 import com.activequant.domainmodel.TimeFrame;
 import com.activequant.interfaces.archive.IArchiveFactory;
 import com.activequant.interfaces.transport.ITransportFactory;
+import com.activequant.transport.activemq.ActiveMQTransportFactory;
 
 /**
  * sample component that downloads data from quandl and stores it into the
@@ -20,57 +22,62 @@ import com.activequant.interfaces.transport.ITransportFactory;
  */
 public class QuandlDownloaderComponent extends ComponentBase {
 
-	public QuandlDownloaderComponent(ITransportFactory transFac, final IArchiveFactory archFactory)
-			throws Exception {
-		super("YahooDownloader", transFac);
+	public static void main(String[] args) throws Exception {
+		ITransportFactory t = new ActiveMQTransportFactory("localhost", 61616);
+		IArchiveFactory a = new HBaseArchiveFactory("localhost");
+		new QuandlDownloaderComponent(t, a);
+	}
 
-		Runnable r = new Runnable() {
-			public void run() {
-				while (true) {
-					try {
-						Thread.sleep(1000 * 60);
-						// check if it is a new day.
+	public QuandlDownloaderComponent(ITransportFactory transFac,
+			final IArchiveFactory archFactory) throws Exception {
+		super("QuandlDownloader", transFac);
+		//
+		// Runnable r = new Runnable() {
+		// public void run() {
+		// while (true) {
+		// try {
+		// Thread.sleep(1000 * 60);
+		// check if it is a new day.
 
-						File f = new File("quandl_symbols.csv");
-						if (f.exists()) {
-							//
-							BufferedReader br = new BufferedReader(
-									new FileReader(f));
-							String l = br.readLine();
-							while (l != null) {
-								// http://www.quandl.com/api/v1/datasets/OFDP/ALUMINIUM_21.csv?
-								String url = "http://www.quandl.com/api/v1/datasets/"
-										+ l + ".csv?";
-								URL u = new URL(url);
-								BufferedReader br2 = new BufferedReader(
-										new InputStreamReader(u.openStream()));
+		File f = new File("quandl_symbols.csv");
+		if (f.exists()) {
+			//
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String l = br.readLine();
+			while (l != null) {
+				// example: http://www.quandl.com/api/v1/datasets/OFDP/ALUMINIUM_21.csv?
+				String url = "http://www.quandl.com/api/v1/datasets/" + l
+						+ ".csv?";
+				URL u = new URL(url);
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(
+						u.openStream()));
 
-								String inputLine;
-								StringBuffer sb = new StringBuffer();
-								while ((inputLine = br2.readLine()) != null) {
-									sb.append(inputLine).append("\n");
-								}
-								br2.close(); 
-								// now that we have all data in our string buffer, let's parse it. 
-								archFactory.getWriter(TimeFrame.EOD);
-								// have to parse, have to write it ... 
-								// trivial three lines. up to you. 
-								// 
-								
-								
-								l = br.readLine();
-							}
-
-						}
-						//
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
+				String inputLine;
+				StringBuffer sb = new StringBuffer();
+				while ((inputLine = br2.readLine()) != null) {
+					sb.append(inputLine).append("\n");
 				}
+				br2.close();
+				// now that we have all data in our string buffer, let's parse
+				// it.
+				archFactory.getWriter(TimeFrame.EOD);
+				// have to parse, have to write it ...
+				// trivial three lines. up to you.
+				//
+
+				l = br.readLine();
 			}
-		};
-		Thread t = new Thread(r);
-		t.start();
+
+		}
+		//
+		// } catch (Exception ex) {
+		// ex.printStackTrace();
+		// }
+		// }
+		// }
+		// };
+		// Thread t = new Thread(r);
+		// t.start();
 	}
 
 	@Override
