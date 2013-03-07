dbc4d9a21b2953713f3f027226b03aaabfa041e9
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2013-03-07 12:51:11 +0000

    cleaning up
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@1005 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- base/src/main/java/com/activequant/utils/ActiveQuantDataField.java
+++ base/src/main/java/com/activequant/utils/ActiveQuantDataField.java
@@ -1,41 +1,52 @@
 package com.activequant.utils;
 
+/**
+ * DEPRECATED. Was used and isn't used any more, I think.
+ * 
+ * @author GhostRider
+ * 
+ */
+
+@Deprecated
 public enum ActiveQuantDataField {
 
-    // will have to load this from DB at one point.
-    BID(Double.class, "BID"), ASK(Double.class, "ASK"), BIDVOL(Double.class, "BIDVOL"), ASKVOL(Double.class, "ASKVOL"), EXP_DT(
-            Long.class, "EXP_DT"), SETTLEMENTDATE(Long.class, "SETTLEMENTDATE"), GATEWAYTIMESTAMP(Long.class,
-            "GATEWAYTIMESTAMP"), CREATED(Long.class, "CREATED"), NAME(String.class, "NAME"), ROLLDATE(Long.class,
-            "ROLLDATE"), SEC_TYP(String.class, "SEC_TYP"), FIRSTTRADINGDATE(Long.class, "FIRSTTRADINGDATE");
+	// will have to load this from DB at one point.
+	BID(Double.class, "BID"), ASK(Double.class, "ASK"), BIDVOL(Double.class,
+			"BIDVOL"), ASKVOL(Double.class, "ASKVOL"), EXP_DT(Long.class,
+			"EXP_DT"), SETTLEMENTDATE(Long.class, "SETTLEMENTDATE"), GATEWAYTIMESTAMP(
+			Long.class, "GATEWAYTIMESTAMP"), CREATED(Long.class, "CREATED"), NAME(
+			String.class, "NAME"), ROLLDATE(Long.class, "ROLLDATE"), SEC_TYP(
+			String.class, "SEC_TYP"), FIRSTTRADINGDATE(Long.class,
+			"FIRSTTRADINGDATE");
 
-    private Class<?> clazz;
-    private String keyName;
+	private Class<?> clazz;
+	private String keyName;
 
-    ActiveQuantDataField(Class<?> clazz, String keyName) {
-        this.clazz = clazz;
-        this.keyName = keyName;
-    }
+	ActiveQuantDataField(Class<?> clazz, String keyName) {
+		this.clazz = clazz;
+		this.keyName = keyName;
+	}
 
-    public Class<?> getClazz() {
-        return clazz;
-    }
+	public Class<?> getClazz() {
+		return clazz;
+	}
 
-    public String keyName() {
-        return keyName;
-    }
+	public String keyName() {
+		return keyName;
+	}
 
-    public Object getValueObject(String textRepresentation) {
-        if (getClazz().equals(Double.class)) {
-            return Double.parseDouble(textRepresentation);
-        } else if (getClazz().equals(Integer.class)) {
-            return Integer.parseInt(textRepresentation);
-        } else if (getClazz().equals(Long.class)) {
-            return Long.parseLong(textRepresentation);
-        } else if (getClazz().equals(String.class)) {
-            return textRepresentation;
-        } else
-            return textRepresentation;
+	public Object getValueObject(String textRepresentation) {
+		if (getClazz().equals(Double.class)) {
+			return Double.parseDouble(textRepresentation);
+		} else if (getClazz().equals(Integer.class)) {
+			return Integer.parseInt(textRepresentation);
+		} else if (getClazz().equals(Long.class)) {
+			return Long.parseLong(textRepresentation);
+		} else if (getClazz().equals(String.class)) {
+			return textRepresentation;
+		} else
+			return textRepresentation;
 
-    }
+	}
 
 }
