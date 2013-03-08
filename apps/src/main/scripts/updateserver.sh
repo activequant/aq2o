11971e26eb3ac8e38a5f53f1bd3c7d5cb234d739
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2013-03-08 13:48:42 +0000

    correcting script.
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@1013 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

--- apps/src/main/scripts/updateserver.sh
+++ apps/src/main/scripts/updateserver.sh
@@ -1,6 +1,9 @@
+echo "Updating server ..."
 rm aq2o-apps-2.2-SNAPSHOT-bundle.zip
 wget http://user:user@server.activequant.com/downloads/aq2o-apps-2.2-SNAPSHOT-bundle.zip
-unzip aq2o-apps-2.2-SNAPSHOT-bundle.zip *.jar
+unzip aq2o-apps-2.2-SNAPSHOT-bundle.zip aq2o/*.jar
 mv aq2o/* .
 rmdir aq2o
 rm aq2o-apps-2.2-SNAPSHOT-bundle.zip
+
+echo "Done. Please restart"
