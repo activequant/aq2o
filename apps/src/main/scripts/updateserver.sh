e7af4c6ddb9f2ceb0be04df86adbe7efa96467f1
ustaudinger <ustaudinger@01b8edfa-0b2f-4ad0-b428-228ee2dd3b63>
2013-03-08 06:39:53 +0000

    adding updateserver.sh script
    git-svn-id: svn://activequant.com/svn/repositories/aq2o/trunk@1007 01b8edfa-0b2f-4ad0-b428-228ee2dd3b63

---
+++ apps/src/main/scripts/updateserver.sh
@@ -0,0 +1,6 @@
+rm aq2o-apps-2.2-SNAPSHOT-bundle.zip
+wget http://user:user@server.activequant.com/downloads/aq2o-apps-2.2-SNAPSHOT-bundle.zip
+unzip aq2o-apps-2.2-SNAPSHOT-bundle.zip *.jar
+mv aq2o/* .
+rmdir aq2o
+rm aq2o-apps-2.2-SNAPSHOT-bundle.zip
