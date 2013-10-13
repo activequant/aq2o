4bd143a4f86531353f1b92a180dc06a6263f6cd2
activequant <ustaudinger@activequant.com>
2013-10-13 20:55:48 +0200

    switching over to IEventSources.

--- model/src/main/java/com/activequant/interfaces/blackbox/IBlackBox.java
+++ model/src/main/java/com/activequant/interfaces/blackbox/IBlackBox.java
@@ -1,7 +1,8 @@
 package com.activequant.interfaces.blackbox;
 
+import com.activequant.interfaces.utils.IEventSource;
 import com.activequant.messages.AQMessages;
-import com.activequant.utils.events.Event;
+import com.activequant.messages.AQMessages.BaseMessage;
 
 /**
  * The black box interface holds all methods relevant for using a trading
@@ -21,28 +22,28 @@ public interface IBlackBox {
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.MarketDataSnapshot> quoteEvent();
+	IEventSource<AQMessages.MarketDataSnapshot> quoteEvent();
 
 	/**
 	 * all ticks arrive on this event.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.Tick> tickEvent();
+	IEventSource<AQMessages.Tick> tickEvent();
 
 	/**
 	 * all OHLC data arrives on this event.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.OHLC> ohlcEvent();
+	IEventSource<AQMessages.OHLC> ohlcEvent();
 
 	/**
 	 * market data subscription responses arrive here.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.MDSubscribeResponse> subscriptionResponseEvent();
+	IEventSource<AQMessages.MDSubscribeResponse> subscriptionResponseEvent();
 
 	/**
 	 * All order related events, such as order submitted, order rejected or
@@ -50,34 +51,40 @@ public interface IBlackBox {
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.BaseMessage> orderEvents();
+	IEventSource<AQMessages.BaseMessage> orderEvents();
 
 	/**
 	 * All acccount related messages arrive here.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.AccountDataMessage> accountDataEvent();
+	IEventSource<AQMessages.AccountDataMessage> accountDataEvent();
 
 	/**
 	 * Some servers send out server time, this arrives here.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.ServerTime> serverTimeEvent();
+	IEventSource<AQMessages.ServerTime> serverTimeEvent();
 
 	/**
 	 * Position/Portfolio messages arrive here.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.PositionReport> positionEvent();
+	IEventSource<AQMessages.PositionReport> positionEvent();
 
 	/**
 	 * Order executions arrive here.
 	 * 
 	 * @return
 	 */
-	Event<AQMessages.ExecutionReport> executionEvent();
+	IEventSource<AQMessages.ExecutionReport> executionEvent();
+
+	/**
+	 * All messages that are being sent to the server have to pushed through this method. 
+	 * @param bm
+	 */
+	void send(BaseMessage bm);	
 	
 }
